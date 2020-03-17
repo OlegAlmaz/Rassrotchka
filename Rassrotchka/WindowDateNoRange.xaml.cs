2cb99e4a80f01099ebb81d4bc829b322895f41e3
unknown <d19-Osheiko@1232-k717-01.lg.sta>
2020-03-17 11:02:54 +0300

    Создано новое окно уведомлений с файлом программного кода

---
+++ Rassrotchka/WindowDateNoRange.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rassrotchka
+{
+	/// <summary>
+	/// Логика взаимодействия для WindowDateNoRange.xaml
+	/// Класс вспывающего окна, когда дата решения сомнительна вне реального интервала
+	/// </summary>
+	public partial class WindowDateNoRange : Window
+	{
+		public WindowDateNoRange()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void ButtonYes_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+	}
+}
