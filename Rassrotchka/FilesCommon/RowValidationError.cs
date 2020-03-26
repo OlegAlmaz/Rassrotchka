412255766e0697b24b86fde7852218170ecc88b7
unknown <d19-Osheiko@1232-k717-01.lg.sta>
2020-03-26 16:09:08 +0300

    Добавлен метод private bool PaysCounValidation(DataRow row)

--- Rassrotchka/FilesCommon/RowValidationError.cs
+++ Rassrotchka/FilesCommon/RowValidationError.cs
@@ -8,83 +8,119 @@ namespace Rassrotchka.FilesCommon
 {
 	public class RowValidationError
 	{
+		private const string DtDec = "4"; //колонка даты решения
+		private const string DtFrs = "8"; //колонка даты решения
+		private const string DtEnd = "9"; //колонка даты решения
+		private const string TypDec = "12"; //колонка типа решения (отсрочка либо отсрочка)
+		private const string PayCnt = "10";//колонка количества платежей рассрочки
+		private const string SumDcs = "6"; //колонка cуммы по решение о рассрочке или отсрочке
+		private const string SumPay = "11"; //колонка cуммы по решение о рассрочке или отсрочке
+
 		/// <summary>
 		/// Таблица из файла excel
 		/// </summary>
 		public DataTable TableFile { get; set; }
 
-		protected bool Flag { get; private set; }
+		/// <summary>
+		/// уведомление об ошибке исли значение равно true
+		/// </summary>
+		public bool IsError { get; private set; }
 
 		public RowValidationError()
 		{
 			TableFile = new DataTable();
-			Flag = true;
+			IsError = true;
 		}
 
 		public bool ValidationError(DataRow row)
 		{
 			//проверка всех дат
-			DatesValidation(row);
+			IsError = DatesValidation(row);
 
-			PaysCounValidation(row);
-			return Flag;
+			IsError = PaysCounValidation(row);
+
+			if (IsError)
+			{
+				row.RowError = @"Ошибка в строке";
+				row.SetModified();
+			}
+			return IsError;
 		}
 
 		//проверка дат решения, а также первой и последней уплаты на соответствие
-		private void DatesValidation(DataRow row)
+		//Если ошика в строке, получаем значение true
+		private bool DatesValidation(DataRow row)
 		{
-			//Проверка даты первой уплаты
-			var dateDecis = (DateTime) row["4"]; //дата решения
-			var dateFirst = (DateTime) row["8"]; //дата первой уплаты
-			var dateEnd = (DateTime) row["9"]; //дата последней уплаты
+			bool isError = false;//если нет ошибок
+
+			var dateDecis = (DateTime) row[DtDec]; //дата решения
+			var dateFirst = (DateTime) row[DtFrs]; //дата первой уплаты
+			var dateEnd = (DateTime) row[DtEnd]; //дата последней уплаты
+
+			//===========Проверка даты первой уплаты
 
 			//сравниваем, в следующем ли месяце будет дата первой уплаты рассрочки
-			bool flag = (string) row["12"] == "рассрочка" && dateDecis.AddMonths(1).Month == dateFirst.Month;
-			if (!(flag))
-				AddErrorToTable(row, @"Ошибка в дате первого платежа по рассрочке!");
+			if ((string)row[TypDec] == "рассрочка" && dateDecis.AddMonths(1).Month != dateFirst.Month)
+			{
+				row.SetColumnError(DtFrs, @"Ошибка в дате первого платежа по рассрочке!");
+				isError = true;
+			}
+			//Сравниваем не меньше либо равна дата уплаты по отсрочке по отношению к дате решения
+			if ((string)row[TypDec] == "отсрочка" && dateFirst <= dateDecis)
+			{
+				row.SetColumnError(DtFrs, @"Ошибка в дате первого платежа по отсрочке!");
+				isError = true;
+			}
 
-			//Сравниваем не меньше либо равна дата уплаты по рассрочке по отношению к дате решения
-			flag = (string) row["12"] == "отсрочка" && dateDecis < dateFirst;
-			if (!flag)
-				AddErrorToTable(row, @"Ошибка в дате первого платежа по отсрочке!");
+			//===========Проверка даты последней уплаты
 
 			//проверка даты последней уплаты при отсрочке
-			flag = (string) row["12"] == "отсрочка" && dateEnd != dateFirst;
-			if (!flag)
-				AddErrorToTable(row, @"Ошибка в дате последнего платежа по отсрочке!");
-
+			if ((string)row[TypDec] == "отсрочка" && dateEnd != dateFirst)
+			{
+				row.SetColumnError(DtEnd, @"Ошибка в дате последнего платежа по отсрочке!");
+				isError = true;
+			}
 			//проверка даты последней уплаты при рассрочке
-			flag = (string) row["12"] == "рассрочка" && dateEnd > dateFirst;
-			if (!flag)
-				AddErrorToTable(row, @"Ошибка в дате последнего платежа по рассрочке!");
+			if ((string)row[TypDec] == "рассрочка" && dateEnd <= dateFirst)
+			{
+				row.SetColumnError(DtEnd, @"Ошибка в дате последнего платежа по рассрочке!");
+				isError = true;
+			}
+			return isError;
 		}
 
-		private void PaysCounValidation(DataRow row)
+		//Проверка количества платежей в случае рассрочки проверка значенич колоники "10".
+		//Проверка значения суммы ежемесячного платежа (колонка "11").
+		private bool PaysCounValidation(DataRow row)
 		{
-			//throw new NotImplementedException();
-		}
+			bool isError = false;//если нет ошибок
+			var dateDecis = (DateTime) row[DtDec]; //дата решения
+			var dateEnd = (DateTime) row[DtEnd]; //дата первой уплаты
+			var sumDecis = Convert.ToDecimal(row[SumDcs].ToString());//cумма по решению
 
+			//=========Проверка количества платежей
 
-		private void AddErrorToTable(DataRow row, string messError)
-		{
-			DataRow newRow = TableFile.NewRow();
-			if (row.HasErrors == false)
+			//должно быть количество месяце отсрочки либо рассрочки (количество платеже отсрочки)
+			int expected = (dateEnd.Year - dateDecis.Year)*12 + (dateEnd.Month - dateDecis.Month);
+
+			int actual = Convert.ToInt32(row[PayCnt].ToString());// по факту
+
+			if (actual != expected)
 			{
-				row.RowError = messError;
-				for (int i = 0; i < row.Table.Columns.Count; i++)
-				{
-					newRow[i] = row[i];
-				}
-				TableFile.Rows.Add(newRow);
-			}
-			else
+				row.SetColumnError(PayCnt, @"Ошибка в количестве месяцев действия рассрочки либо отсрочки");
+				isError = true;
+
+			}			
+
+			//===============Проверка суммы ежемесячного платежа
+			decimal sumPayExpected = sumDecis/expected; //расчетное значение
+			decimal sumPayActual = Convert.ToDecimal(row[SumPay].ToString()); // по факту
+			if (Math.Abs(sumPayExpected - sumPayActual) >= 1000)
 			{
-				row.RowError = row.RowError + string.Format("\t{0}", messError);
-				//DataRow rowTemp = TableFile.Rows.Find(row["0"]);
-				int ind = TableFile.Rows.IndexOf(TableFile.Rows.Find(row["0"]));
-				TableFile.Rows[ind].RowError = row.RowError;
+				row.SetColumnError(SumPay, @"Ошибка в cумме ежемесячного платежа, расчетное значение: " + sumPayExpected);
+				isError = true;
 			}
-			Flag = false;
+			return isError;
 		}
 	}
 }
