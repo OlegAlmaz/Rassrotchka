a9ea8798508ab08a1bb6f7db99073bdafefd77c9
d19-Osheiko <d19-Osheiko@DC01-10487049>
2020-05-28 10:29:38 +0300

    Создание классов

---
+++ Rassrotchka/FactoryForecast20.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Rassrotchka
+{
+	/// <summary>
+	/// формирует сводные данные о прогноозе поступлений по рассрочке на текущий месяц
+	/// в разрезе плательщиков и платежей на 1 число
+	/// </summary>
+	public class FactoryForecast20 : AbstractFactory
+	{
+		public override AbstractTable CreateTable()
+		{
+			return new TableForecast20();
+		}
+
+		public override AbstractFile CreateFile()
+		{
+			return new FileForecast20();
+		}
+	}
+
+	public class TableForecast20 : AbstractTable
+	{
+
+		protected override SqlDataAdapter SqlDataAdapter(SqlConnection sqlConnection)
+		{
+			var adapter = new SqlDataAdapter(BaseElementName.ProcedGeNPayFromPlatej, sqlConnection)
+			{
+				SelectCommand = { CommandType = CommandType.StoredProcedure }
+			};
+			adapter.SelectCommand.Parameters.Add("@DateStart", SqlDbType.DateTime);
+			adapter.SelectCommand.Parameters[0].SqlValue = Args.DateFirst;
+			adapter.SelectCommand.Parameters.Add("@DateEnd", SqlDbType.DateTime);
+			adapter.SelectCommand.Parameters[1].SqlValue = Args.DateEnd;
+			return adapter;
+		}
+
+	}
+
+	public class FileForecast20 : AbstractFile
+	{
+
+		public override void Interact(AbstractTable tb)
+		{
+			FilePath = "FilesTemplates\\Рассрочки_налоги.xlsx";
+			NewFile = string.Format(@"d:\Мои документы\Рассрочки\!Учет поступлений по рассрочке\Рассрочка_налоги_{0}.xlsx",
+									tb.Args.Monthname.Imenit);
+			HeaderFile.NameCellHeader = "A1";
+			HeaderFile.Header = string.Format(@"Прогноз поступлений за счет погашения рассроченных и отсроченых сумм в {0} 2020 года",
+										tb.Args.Monthname.Roditel);
+			TableCellsNames.HeaderCollumns = "A2";
+			TableCellsNames.CellData = "A3";
+
+			DateToFile(tb);
+		}
+	}
+}
