6f892fc39dbfa7207692cb97c741053cb5118e5d
d19-Osheiko <d19-Osheiko@DC01-10487049>
2020-05-28 10:30:42 +0300

    Промежуточная фиксация

--- Rassrotchka/Client.cs
+++ Rassrotchka/Client.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
+using GemBox.Spreadsheet;
 using Rassrotchka.FilesCommon;
 using Rassrotchka.Properties;
 
@@ -19,35 +16,11 @@ namespace Rassrotchka
 			_file = factory.CreateFile();
 		}
 
-		public void Run(object argument)
+		public void Run(ArgumentDebitPay args)
 		{
-			_file.Interact(argument, _table);
+			_table.Args = args;
+			_file.Interact(_table);
 		}
-	}
-
-	public abstract class AbstractTable
-	{
-		public abstract DataSet SqlToDataSet(ArgumentDebitPay args);
-
-		public DataSet FillDataSet(string sqlCommandText)
-		{
-			using (var sqlConnection = new SqlConnection(Settings.Default.NedoimkaConnectionString))
-			{
-				using (var dataSet = new DataSet())
-				{
-					var sqlDataAdapter = new SqlDataAdapter(sqlCommandText, sqlConnection);
-					sqlDataAdapter.Fill(dataSet);
-					return dataSet;
-				}
-			}
-		}
-
-	}
-
-	public abstract class AbstractFile
-	{
-		//public FilterOPF FilterOpf;
-		public abstract void Interact(object argument, AbstractTable tb);
 
 	}
 
@@ -56,5 +29,80 @@ namespace Rassrotchka
 		public abstract AbstractTable CreateTable();
 		public abstract AbstractFile CreateFile();
 	}
+
+	public abstract class AbstractTable
+	{
+		public ArgumentDebitPay Args;
+
+		public DataSet SqlToDataSet()
+		{
+			var sqlConnection = new SqlConnection(Settings.Default.NedoimkaConnectionString);
+			using (var dataSet = new DataSet())
+			{
+				//заполнение сводной таблицы
+				var sqlDataAdapter = SqlDataAdapter(sqlConnection);
+				sqlDataAdapter.Fill(dataSet);
+				return dataSet;
+			}
+		}
+
+		protected abstract SqlDataAdapter SqlDataAdapter(SqlConnection sqlConnection);
+	}
+
+	public abstract class AbstractFile
+	{
+		public string FilePath { get; set; }
+		public string NewFile { get; set; }
+
+		//public FilterOPF FilterOpf;
+		public abstract void Interact(AbstractTable tb);
+
+		/// <summary>
+		/// Формирует и копирует данные в excel файл на одном листе
+		/// </summary>
+		/// <param name="tb"></param>
+		protected void DateToFile(AbstractTable tb)
+		{
+			string licensi = tb.Args.ExcelParametrs.License;
+			SpreadsheetInfo.SetLicense(licensi);
+			var workbook = ExcelFile.Load(FilePath);
+			var ws = workbook.Worksheets[0];
+			DataTable dataTable = tb.SqlToDataSet().Tables[0];
+			var options = new InsertDataTableOptions(TableCellsNames.CellData);
+			ws.InsertDataTable(dataTable, options);
+			ws.Cells[HeaderFile.NameCellHeader].Value = HeaderFile.Header;
+			FormattingRangeGemBox.FormattingUsedRange(ws, TableCellsNames.CellData);
+			workbook.Save(NewFile);
+		}
+
+		/// <summary>
+		/// Содержит содержание заголовка файла и имя ячейки, в которое он помещается
+		/// </summary>
+		protected static class HeaderFile
+		{
+			public static string Header = string.Empty;
+			/// <summary>
+			/// имя ячейки в которую помещается заголовок файла, по умолчанию "A1"
+			/// </summary>
+			public static string NameCellHeader = "A1";
+		}
+
+		/// <summary>
+		/// Содержит имена ячеек, в которых помещаются названия колонок таблицы 
+		/// и непосредственно данные таблицы
+		/// </summary>
+		protected static class TableCellsNames
+		{
+			/// <summary>
+			/// первая ячейка строки с названиями колонок, по умолчанию - "A2"
+			/// </summary>
+			public static string HeaderCollumns = "A2";
+			/// <summary>
+			/// первая ячейка таблицы с данными, по умолчанию - "A3"
+			/// </summary>
+			public static string CellData = "A3";
+		}
+	}
+
 
 }
