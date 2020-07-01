da518a78fbaf25c4928a2c0baf0a556792feb846
d19-Osheiko <d19-Osheiko@DC01-10487049>
2020-07-01 15:28:44 +0300

    Добавленн класс

---
+++ RassrotchkaTests/FilesCommon/FilterClassTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rassrotchka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rassrotchka.Tests
+{
+    [TestClass()]
+    public class FilterClassTests
+    {
+        [TestMethod()]
+        public void FilterStringTest()
+        {
+            FilterClass filter = new FilterClass();
+            filter.Close = "true";
+            filter.Kod_GNI = "2";
+            string expected = "WHERE Close = true AND Kod_GNI = 2";
+
+            Assert.AreEqual(expected, filter.FilterString());
+        }
+
+        [TestMethod()]
+        public void FilterStringTest2()
+        {
+            FilterClass filter = new FilterClass();
+            filter.Close = "true";
+            filter.Name = "ЛЭС";
+            string expected = "WHERE Close = true AND Name LIKE %ЛЭС%";
+
+            Assert.AreEqual(expected, filter.FilterString());
+        }
+        [TestMethod()]
+        public void FilterStringTest3()
+        {
+            FilterClass filter = new FilterClass();
+            filter.Close = "true";
+            filter.Kod_Payer = "1111";
+            string expected = "WHERE Close = true AND Kod_Payer = 1111";
+
+            Assert.AreEqual(expected, filter.FilterString());
+        }
+        [TestMethod()]
+        public void FilterStringTest4()
+        {
+            FilterClass filter = new FilterClass();
+            filter.Close = "true";
+            filter.Kod_GNI = "2";
+            filter.Name = "ЛЭС";
+            string expected = "WHERE Close = true AND Kod_GNI = 2 AND Name LIKE %ЛЭС%";
+
+            Assert.AreEqual(expected, filter.FilterString());
+        }
+    }
+}
