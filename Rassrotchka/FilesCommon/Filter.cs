8bbea8e43cee7bf547ebef70074e96d5e0af3134
d19-Osheiko <d19-Osheiko@DC01-10487049>
2020-07-01 15:22:18 +0300

    Доработан класс

--- Rassrotchka/FilesCommon/Filter.cs
+++ Rassrotchka/FilesCommon/Filter.cs
@@ -9,22 +9,19 @@ namespace Rassrotchka
     {
         #region Поля фильтра
         private string _close;
-        private short _kod_GNI;
-        private long _kod_Payer;
+        private string _kod_GNI;
+        private string _kod_Payer;
         private string _name;
+        private readonly string[] _arrCond = new[]
+                                        {
+                                            "",//_close     0
+                                            "",//_kod_GNI   1
+                                            "",//_kod_Payer 2
+                                            ""//_name       3
+                                        };
         #endregion
 
-        private string _filter;
-        private string[] _arrCond = new[]
-                                        {
-                                            "",//_close
-                                            "",//_kod_GNI
-                                            "",//_kod_Payer
-                                            ""//_name
-                                        };
-
-        private const string _whereStr = "WHERE ";
-
+        #region Свойства
         public string Close
         {
             get => _close;
@@ -38,38 +35,65 @@ namespace Rassrotchka
             }
         }
 
-        public short Kod_GNI { get => _kod_GNI; set => _kod_GNI = value; }
+        public string Kod_GNI
+        {
+            get => _kod_GNI;
+            set
+            {
+                if (_kod_GNI != value)
+                {
+                    _kod_GNI = value;
+                    _arrCond[1] = _kod_GNI != string.Empty ? $"Kod_GNI = {_kod_GNI}" : string.Empty;
+                }
+            }
+        }
 
-        public long Kod_Payer
+        public string Kod_Payer
         {
             get => _kod_Payer; set
             {
                 if (_kod_Payer != value)
                 {
                     _kod_Payer = value;
-                    _arrCond[2] = _kod_Payer != 0 ? $"Kod_Payer = {_kod_Payer}" : string.Empty;
+                    _arrCond[2] = _kod_Payer != string.Empty ? $"Kod_Payer = {_kod_Payer}" : string.Empty;
                 }
             }
         }
 
-        public string Name { get => _name; set => _name = value; }
-        public string Filter { get => _filter; private set => _filter = value; }
-
-        private void Onchanged()
+        public string Name
         {
-            _filter = string.Empty;
-            int ind = 0;//todo уточнить, возможно ошибка с местом инициализации
+            get => _name;
+            set 
+            { if (_name != value)
+                {
+                    _name = value;
+                    _arrCond[3] = _name != string.Empty ? $"Name LIKE %{_name}%" : string.Empty;
+                }
+            }
+        }
+
+        public string Filter { get; set; }
+        #endregion
+        /// <summary>
+        /// Возвращает строку фильтра
+        /// </summary>
+        /// <returns>строка фильтра</returns>
+        public string FilterString()
+        {
+            Filter = string.Empty;
+            int cound = 1;//todo уточнить, возможно ошибка с местом инициализации
             for (int i = 0; i < _arrCond.Length; i++)
             {
                 if (_arrCond[i] != string.Empty)
                 {
-                    if (ind == 0)
-                        _filter = $"WHERE {_arrCond[i]}";
+                    if (cound == 1)
+                        Filter = _arrCond[i];
                     else
-                        _filter += $" AND {_arrCond[i]}";
+                        Filter += $" AND {_arrCond[i]}";
+                    cound++;
                 }
-                ind++;
             }
+            return Filter;
         }
     }
 }
