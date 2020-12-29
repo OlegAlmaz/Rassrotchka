e0e6f5721d95d4df93ba6d8f3a7cbb46c203c827
d19-Osheiko <d19-Osheiko@DC01-10487049>
2020-12-29 11:41:43 +0300

    добавлен класс

---
+++ Rassrotchka/Converters/CountRowConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Rassrotchka.Converters
+{
+    public class CountRowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                int rowsCount = (int)value;
+                if (!MainWindow.IsDirty)
+                {
+                    --rowsCount;
+                }
+                if (rowsCount == 0)
+                {
+                    return "";
+                }
+                return rowsCount;
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
